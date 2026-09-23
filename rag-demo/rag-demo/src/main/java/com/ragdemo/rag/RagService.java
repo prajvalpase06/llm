b6f9a316cf7f105e1d1e718fc542c36df0026f29
Prajval Pase <prajvalpase06@gmail.com>
2026-09-23 10:24:50 +0530

    ragdemo: chatClient integration

--- rag-demo/rag-demo/src/main/java/com/ragdemo/rag/RagService.java
+++ rag-demo/rag-demo/src/main/java/com/ragdemo/rag/RagService.java
@@ -5,6 +5,7 @@ import org.springframework.ai.document.Document;
 import org.springframework.ai.vectorstore.SearchRequest;
 import org.springframework.ai.vectorstore.pgvector.PgVectorStore;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.ai.chat.prompt.*;
 import org.springframework.core.io.Resource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -12,6 +13,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.List;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 import static reactor.netty.http.HttpConnectionLiveness.log;
@@ -42,16 +44,52 @@ public class RagService {
         // 1. Retrieve relevant documents
         SearchRequest searchRequest = SearchRequest.builder().query(message).topK(4).build();
         List<Document> similarDocuments = vectorStore.doSimilaritySearch(searchRequest);
-//        String information = similarDocuments.stream().map(Document::getText).collect(Collectors.joining("\n\n--- CHUNK ---\n\n"));
 
-        for (Document document : similarDocuments) {
-            log.info("----- DOCUMENT -----");
-            log.info("Text: {}", document.getText());
-            log.info("Metadata: {}", document.getMetadata());
+        // 4. Combine retrieved chunks into context
+        String context = similarDocuments.stream()
+                        .map(Document::getText)
+                        .collect(Collectors.joining("\n\n---CHUNK----\n\n"));
+
+        // 5. Send question + retrieved context to the LLM
+        SystemPromptTemplate systemPromptTemplate = new SystemPromptTemplate(ragPromptTemplate);
+        Prompt prompt = new Prompt(List.of(systemPromptTemplate.createMessage(Map.of("question", message,
+                                                                                     "context", context))));
+//        return chatClient.prompt(prompt).call().content();
+
+        var response = chatClient.prompt(prompt)
+
+                .call();
+
+        var chatResponse = response.chatResponse();
+
+        System.out.println("CHAT RESPONSE = " + chatResponse);
+
+        if (chatResponse == null) {
+
+            return "ChatResponse is null";
+
         }
 
-        return similarDocuments.getFirst().getText();
+        System.out.println("RESULT = " + chatResponse.getResult());
+
+        if (chatResponse.getResult() == null) {
+
+            return "ChatResponse result is null";
+
+        }
+
+        System.out.println("OUTPUT = " + chatResponse.getResult().getOutput());
+
+        if (chatResponse.getResult().getOutput() == null) {
+
+            return "ChatResponse output is null";
+
+        }
+
+        System.out.println("TEXT = " +
+
+                chatResponse.getResult().getOutput().getText());
+
+        return chatResponse.getResult().getOutput().getText();
     }
-
-
 }
